028eeed04b008e6f2b1df1764cffaebe82c6af4a
zhangjun <zhangjunhello@qq.com>
2020-07-13 21:56:36 +0800

    add main.go

--- vars/main.go
+++ vars/main.go
@@ -1,6 +1,9 @@
 package vars
 
+import "strings"
+
 func Mmm(i int) string {
-	var r1 string = a + string(b) + string(i)
+	var str []string = []string{a, string(b), string(i)}
+	var r1 string = strings.Join(str, "")
 	return string(r1)
 }
