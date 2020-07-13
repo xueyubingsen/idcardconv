a05ec85994b5a67c8c99fee188eb984e2dab07b4
zhangjun <zhangjunhello@qq.com>
2020-07-13 22:34:02 +0800

    bug fix

--- vars/main.go
+++ vars/main.go
@@ -1,9 +1,12 @@
 package vars
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 func Mmm(i int) string {
-	var str []string = []string{a, string(b), string(i)}
+	var str []string = []string{a, strconv.Itoa(b), strconv.Itoa(i)}
 	var r1 string = strings.Join(str, "")
 	return string(r1)
 }
